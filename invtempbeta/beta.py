80008fc918d456576b684410e771d83a9661fcd4
Ivan Mikhailov <mikhailov.ivan.imc@gmail.com>
2023-01-12 01:29:46 +0300

    Add tests for fgamma, W, newton_raphson

--- invtempbeta/beta.py
+++ invtempbeta/beta.py
@@ -2,8 +2,6 @@ import numpy as np
 import warnings
 
 
-warnings.filterwarnings('ignore') # to suppress warnings
-
 def fgammaL(x):
     return 2. * x / (1. - 4. * x + np.sqrt((1. - 6. * x) * (1. - 2. * x)))
 
@@ -12,13 +10,14 @@ def W(beta,v):
 
 def newton_raphson(fun, x, h=1e-8):
     """ Newton-Raphson's method for the argument iteration """
+    warnings.filterwarnings('ignore') # to suppress warnings
     try:
         return  x - 2.0 * h * fun(x) / (fun(x+h) - fun(x-h))
     except ZeroDivisionError:
         return  x - 2.0 * h 
 
 def zero_of_function(fun, x0=0.5, precision=10):
-    """ finding the zero of function f(x) on the interval (x0,xm) """ 
+    """ finding the zero of function f(x) closest to x0 """ 
     h = 10.0**(-precision)
     x = 1.0
     x_new = x0
@@ -42,12 +41,14 @@ class BetaCalc:
         u = np.fft.ifft(Gv_L).real
         self.G_L = np.array([np.roll(u, i) for i in range(n)])
         
-    def beta(self, v, x0=0.5):
-        assert len(v) == self.n
+    def beta(self, v, *kwargs):
+        if len(v) != self.n:
+            raise ValueError("input length v != input n in BetaCalc instance")
         R = lambda beta: self.P @ W(beta,v)
         f = lambda beta: np.linalg.det(self.E - 4.0 * self.x * R(beta) - self.x * self.G_L @ R(beta))
-        return zero_of_function(f,x0=x0)
+        return zero_of_function(f,*kwargs)
 
 
 if __name__ == '__main__':
-    print(fgammaL(0.5))
+
+    print(np.isnan(newton_raphson(lambda x: np.sqrt(x), -1., h=1e-9)))
--- tests/test_beta.py
+++ tests/test_beta.py
@@ -1,5 +1,18 @@
-from invtempbeta.beta import fgammaL
+from invtempbeta.beta import (fgammaL, W, newton_raphson)
+import numpy as np
 
 
 def test_fgammaL():
-    assert fgammaL(0.5) == -1.0
+    assert fgammaL(0.5)  == -1.0
+    assert fgammaL(0.0)  ==  0.0
+    assert fgammaL(-0.5) == -0.1715728752538099
+
+def test_W():
+    assert W(1, [0]) == 1
+    assert np.array(W(0.0, np.array([1., -1.])) == [[1., 0.],[0., 1.]]).all()
+
+def test_newton_raphson():
+    # test small values
+    assert newton_raphson(lambda x: 1./x, 0., h=1e-16) == -2e-16
+    # test suppress warnings
+    assert np.isnan(newton_raphson(lambda x: np.sqrt(x), -1.))
